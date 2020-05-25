f312fd8d88bfac589395d7349316220ff1edd32c
Colburn Hassman <colburn7@vt.edu>
2020-05-25 19:27:14 +0200

    Calculated USO returns and errors. Made USO graph

--- USO_Analysis.R
+++ USO_Analysis.R
@@ -20,3 +20,13 @@ USO <- read_excel("G:/My Drive/3_Massa Research/Neff Paper/Working_Folder/Data_U
 USO <- USO[order(USO$DATE),]
 
 #--------------------Calculate Returns and Errors ---------------------------#
+USO$per_asset_return <- log(USO$Futures/lag(USO$Futures))
+USO$per_ETF_return <- log(USO$USO_MID /lag(USO$USO_MID))
+USO$per_NAV_return <- log(USO$USO_NAV/lag(USO$USO_NAV))
+USO$etf_asset_error <- USO$per_ETF_return - USO$per_asset_return
+USO <- na.omit(USO)
+
+#---------------------Exploratory Plots--------------------------------------#
+qplot(USO$DATE, (USO$etf_asset_error * 100), geom = 'line') + theme_bw() +
+  ggtitle("Daily Return Error: USO ETF - Asset Basket") + ylab("Error (%)") +
+  xlab("Date")
