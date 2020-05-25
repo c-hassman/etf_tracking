776c371b168076aec667ac169db6020029725be2
Colburn Hassman <colburn7@vt.edu>
2020-05-25 17:40:20 +0200

    WEAT: caculated returns and error, made error plot

--- WEAT_Analysis.R
+++ WEAT_Analysis.R
@@ -21,5 +21,14 @@ WEAT <- WEAT[order(WEAT$DATE),] #order by date
 WEAT$asset_basket <- (WEAT$`F1(.35)` * 0.35) + (WEAT$`F2(.3)` * 0.3) + (WEAT$`F3(.35)` * 0.35) #construct asset baskets
 
 #-----------------------Calculate Returns and Errors--------------#
+WEAT$per_asset_return <- log(WEAT$asset_basket/lag(WEAT$asset_basket)) #calculate percent asset return 
+WEAT$per_ETF_return <- log(WEAT$WEAT_MID/lag(WEAT$WEAT_MID)) #calculate percent ETF return
+WEAT$per_NAV_return <- log(WEAT$WEAT_NAV/lag(WEAT$WEAT_NAV)) #calculate percent NAV return
+WEAT$etf_asset_error <- WEAT$per_ETF_return - WEAT$per_asset_return #calculate percent ETF return
+WEAT <- na.omit(WEAT) #Omit Rows with NAs
 
+#---------------------Exploratory Plots--------------------------#
+qplot(WEAT$DATE, (WEAT$etf_asset_error * 100), geom = 'line') + theme_bw() +
+  ggtitle("Daily Return Error: WEAT ETF - Asset Basket") + ylab("Error (%)") +
+  xlab("Date")
   
