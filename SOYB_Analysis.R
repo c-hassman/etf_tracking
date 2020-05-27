1a8309763aa670aca0a9a4adc40f1e58c334f7c8
Colburn Hassman <colburn7@vt.edu>
2020-05-27 13:10:41 +0200

    SOYB: calculated returns and error, initial plot

--- SOYB_Analysis.R
+++ SOYB_Analysis.R
@@ -18,7 +18,16 @@ SOYB <- read_excel("G:/My Drive/3_Massa Research/Neff Paper/Working_Folder/Data_
                                                  "numeric", "numeric", "numeric", 
                                                  "numeric", "numeric", "numeric"))
 SOYB <- SOYB[order(SOYB$DATE),]
-SOYB$asset_basket <- 
+SOYB$asset_basket <- (SOYB$`F1(.35)` * 0.35) + (SOYB$`F2(.3)` * 0.30) + (SOYB$`F3(.35)` * 0.35)
 
 #-------------------------Calculate Returns and Errors------------------------------#
-SOYB$
+SOYB$per_asset_return <- log(SOYB$asset_basket / lag(SOYB$asset_basket))
+SOYB$per_ETF_return <- log(SOYB$SOYB_MID / lag(SOYB$SOYB_MID))
+SOYB$per_NAV_return <- log(SOYB$SOYB_NAV / lag(SOYB$SOYB_NAV))
+SOYB$etf_asset_error <- SOYB$per_ETF_return - SOYB$per_asset_return
+SOYB <- na.omit(SOYB)
+#---------------------Exploratory Plots--------------------------------------#
+qplot(SOYB$DATE, (SOYB$etf_asset_error * 100), geom = 'line') + theme_bw() +
+  ggtitle("Daily Return Error: SOYB ETF - Asset Basket") + ylab("Error (%)") +
+  xlab("Date")
+
