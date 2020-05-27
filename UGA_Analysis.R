feddfa5e6c567cf37dc2bcf9caddaa7700306e79
Colburn Hassman <colburn7@vt.edu>
2020-05-27 14:42:52 +0200

    UGA: added ETF and Asset Basket Price Plot

--- UGA_Analysis.R
+++ UGA_Analysis.R
@@ -26,6 +26,19 @@ UGA$etf_asset_error <- UGA$per_ETF_return - UGA$per_asset_return
 UGA <- na.omit(UGA)
 
 #----------------------Exploratory Plot--------------------------#
+#------ETF, Asset Basket Error
 qplot(UGA$DATE, (UGA$etf_asset_error * 100), geom = 'line') + theme_bw() +
   ggtitle("Daily Return Error: UGA ETF - Asset Basket") + ylab("Error (%)") +
   xlab("Date")
+#-----ETF and Asset Basket 
+etfcolor <- "black"
+assetcolor <- "darkgrey"
+coeff <- UGA$Futures[1] / UGA$UGA_MID[1]
+ggplot(data = UGA, aes(x = DATE)) +
+  geom_line(aes(y = UGA_MID), color = etfcolor) +
+  geom_line(aes(y = Futures / coeff), color = assetcolor) +
+  scale_y_continuous(
+    name = 'ETF Price ($)',
+    sec.axis = sec_axis(~.*coeff, name = "Asset Basket Price (Dollars per Gallon)")
+) + theme_bw() + ggtitle("UGA ETF and Asset Basket Price") + xlab("Date") 
+
