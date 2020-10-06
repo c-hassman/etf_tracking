8e6de477cc2b9da2085d65b06b9aefa290d13af4
Colburn Hassman <59699214+c-hassman@users.noreply.github.com>
2020-10-06 10:50:42 -0400

    October 5 update
    completely reworking the data source (not forward filling dates, putting in redudancy so that roll dates are properly deleted): redefining tracking error to be the residuals from the ols regression.  Re fit ARMA model for new reisduals.. Stopped to focus on filling out the data section

--- Combined_Analysis.R
+++ Combined_Analysis.R
@@ -9,6 +9,7 @@ library(rugarch)
 library(fpp2)
 library(lubridate)
 library(writexl)
+library(tseries)
 
 #devtools::install_github("ropensci/writexl")
 
@@ -95,38 +96,38 @@ CORN <- CORN[order(CORN$DATE),] #order by date
 CORN$asset_basket <- (CORN$`F1(.35)` * 0.35) + (CORN$`F2(.3)` * 0.3) + (CORN$`F3(.35)` * 0.35) #reconstruct asset basket
 CORN$per_asset_return <- log(CORN$asset_basket/lag(CORN$asset_basket))* 100 # calculate percent asset basket return
 CORN$per_ETF_return <- log(CORN$CORN_MID/lag(CORN$CORN_MID)) * 100#calculate percent ETF return
-CORN$per_NAV_return <- log(CORN$CORN_NAV/lag(CORN$CORN_NAV)) * 100#calculate percent NAV return
-CORN$etf_asset_error <- CORN$per_ETF_return - CORN$per_asset_return #calculate error between ETF and Asset
+#CORN$per_NAV_return <- log(CORN$CORN_NAV/lag(CORN$CORN_NAV)) * 100#calculate percent NAV return
+#CORN$etf_asset_error <- CORN$per_ETF_return - CORN$per_asset_return #calculate error between ETF and Asset
 
 SOYB$DATE <- as.Date(SOYB$DATE, origin = "1899-12-30") # Set date
 SOYB <- SOYB[order(SOYB$DATE),] #order by date
 SOYB$asset_basket <- (SOYB$`F1(.35)` * 0.35) + (SOYB$`F2(.3)` * 0.30) + (SOYB$`F3(.35)` * 0.35)
 SOYB$per_asset_return <- log(SOYB$asset_basket / lag(SOYB$asset_basket)) * 100
 SOYB$per_ETF_return <- log(SOYB$SOYB_MID / lag(SOYB$SOYB_MID)) * 100
-SOYB$per_NAV_return <- log(SOYB$SOYB_NAV / lag(SOYB$SOYB_NAV)) * 100
-SOYB$etf_asset_error <- SOYB$per_ETF_return - SOYB$per_asset_return
+#SOYB$per_NAV_return <- log(SOYB$SOYB_NAV / lag(SOYB$SOYB_NAV)) * 100
+#SOYB$etf_asset_error <- SOYB$per_ETF_return - SOYB$per_asset_return
 
 WEAT$DATE <- as.Date(WEAT$DATE, origin = "1899-12-30")  #set date 
 WEAT <- WEAT[order(WEAT$DATE),] #order by date
 WEAT$asset_basket <- (WEAT$`F1(.35)` * 0.35) + (WEAT$`F2(.3)` * 0.3) + (WEAT$`F3(.35)` * 0.35) #reconstruct asset basket
 WEAT$per_asset_return <- log(WEAT$asset_basket/lag(WEAT$asset_basket))* 100 # calculate percent asset basket return
 WEAT$per_ETF_return <- log(WEAT$WEAT_MID/lag(WEAT$WEAT_MID)) * 100#calculate percent ETF return
-WEAT$per_NAV_return <- log(WEAT$WEAT_NAV/lag(WEAT$WEAT_NAV)) * 100#calculate percent NAV return
-WEAT$etf_asset_error <- WEAT$per_ETF_return - WEAT$per_asset_return #calculate error between ETF and Asset
+#WEAT$per_NAV_return <- log(WEAT$WEAT_NAV/lag(WEAT$WEAT_NAV)) * 100#calculate percent NAV return
+#WEAT$etf_asset_error <- WEAT$per_ETF_return - WEAT$per_asset_return #calculate error between ETF and Asset
 
 USO$DATE <- as.Date(USO$DATE, origin = "1899-12-30") 
 USO <- USO[order(USO$DATE),]
 USO$per_asset_return <- log(USO$Futures/lag(USO$Futures)) * 100
 USO$per_ETF_return <- log(USO$USO_MID /lag(USO$USO_MID)) * 100
-USO$per_NAV_return <- log(USO$USO_NAV/lag(USO$USO_NAV)) * 100
-USO$etf_asset_error <- USO$per_ETF_return - USO$per_asset_return
+#USO$per_NAV_return <- log(USO$USO_NAV/lag(USO$USO_NAV)) * 100
+#USO$etf_asset_error <- USO$per_ETF_return - USO$per_asset_return
 
 UGA$DATE <- as.Date(UGA$DATE, origin = "1899-12-30") 
 UGA <- UGA[order(UGA$DATE),]
 UGA$per_asset_return <- log(UGA$Futures/lag(UGA$Futures)) * 100
 UGA$per_ETF_return <- log(UGA$UGA_MID/lag(UGA$UGA_MID)) * 100
-UGA$per_NAV_return <- log(UGA$UGA_NAV/lag(UGA$UGA_NAV)) * 100
-UGA$etf_asset_error <- UGA$per_ETF_return - UGA$per_asset_return
+#UGA$per_NAV_return <- log(UGA$UGA_NAV/lag(UGA$UGA_NAV)) * 100
+#UGA$etf_asset_error <- UGA$per_ETF_return - UGA$per_asset_return
 
 #  Volume data ------------------------------------------------------------------------
 volume <- read.csv("G:/My Drive/3_Massa Research/Neff Paper/Working_Folder/Volume.csv")
@@ -163,92 +164,124 @@ UGA$volume_return <-log(UGA$Volume/lag(UGA$Volume)) * 100
 #--- More data cleaning
 # The code below handles the issue of roll dates and using a continous time model
 # with things which dont trade on the weekend. 
-#Remove rows with NAsm which has the effect of deleting roll days
-CORN <- na.omit(CORN) 
+#Remove rows with NAsm 
+CORN <- na.omit(CORN)  
+CORN <- CORN[!(CORN$ROLL == 1),] #directly remove roll
 # This creates rows of NA's for all the missing days, including weekends, holidays,
 # and the roll days we just deleted. Not sure why I have to create another "Date"
 # column but that is the only way I could get it to work
-CORN <- CORN %>% 
-  mutate(Date = as.Date(DATE)) %>% 
-  complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
-# Now to forward fill the date
-CORN <- na.locf(na.locf(CORN), fromLast = TRUE)
-# Remove the additional date column
-CORN <- subset(CORN, select = -DATE)
+# CORN <- CORN %>% 
+#   mutate(Date = as.Date(DATE)) %>% 
+#   complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
+# # Now to forward fill the date
+# CORN <- na.locf(na.locf(CORN), fromLast = TRUE)
+#CORN <- subset(CORN, select = -DATE)
 #CORN <- na.omit(CORN) 
 # Convert the object into a XTS object
-CORN.xts <- xts(CORN[,-1], order.by = CORN$Date)
+CORN.xts <- xts(CORN[,-1], order.by = CORN$DATE)
 
 SOYB <- na.omit(SOYB)
-SOYB <-SOYB %>% 
-  mutate(Date = as.Date(DATE)) %>%
-  complete(Date = seq.Date(min(DATE), max(DATE), by = "day"))
-SOYB <- na.locf(na.locf(SOYB), fromLast = TRUE) #forward fill the date
-SOYB <- subset(SOYB, select = -DATE) #remove redundant date column
+SOYB <- SOYB[!(SOYB$ROLL == 1),]
+#SOYB <-SOYB %>% 
+#  mutate(Date = as.Date(DATE)) %>%
+#  complete(Date = seq.Date(min(DATE), max(DATE), by = "day"))
+#SOYB <- na.locf(na.locf(SOYB), fromLast = TRUE) #forward fill the date
+# SOYB <- subset(SOYB, select = -DATE) #remove redundant date column
 SOYB.xts <- xts(SOYB[,-1], order.by = SOYB$Date) #create xts object
 
 WEAT <- na.omit(WEAT) 
-WEAT <- WEAT %>% 
-  mutate(Date = as.Date(DATE)) %>% 
-  complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
-WEAT <- na.locf(na.locf(WEAT), fromLast = TRUE)
-WEAT <- subset(WEAT, select = -DATE)
-WEAT.xts <- xts(WEAT[,-1], order.by = WEAT$Date)
+WEAT <- WEAT[!(WEAT$ROLL == 1),]
+# WEAT <- WEAT %>% 
+#   mutate(Date = as.Date(DATE)) %>% 
+#   complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
+# WEAT <- na.locf(na.locf(WEAT), fromLast = TRUE)
+# WEAT <- subset(WEAT, select = -DATE)
+WEAT.xts <- xts(WEAT[,-1], order.by = WEAT$DATE)
 
 
 USO <- na.omit(USO)
-USO <- USO %>% 
-  mutate(Date = as.Date(DATE)) %>% 
-  complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
-USO <- na.locf(na.locf(USO), fromLast = TRUE)
-USO <- subset(USO, select = -DATE)
-USO.xts <- as.xts(USO, order.by = USO$Date)
+USO <- USO[!(USO$ROLL == 1),]
+
+# USO <- USO %>% 
+#   mutate(Date = as.Date(DATE)) %>% 
+#   complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
+# USO <- na.locf(na.locf(USO), fromLast = TRUE)
+# USO <- subset(USO, select = -DATE)
+USO.xts <- as.xts(USO, order.by = USO$DATE)
 
 UGA <- na.omit(UGA) 
-UGA <- UGA %>% 
-  mutate(Date = as.Date(DATE)) %>% 
-  complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
-UGA <- na.locf(na.locf(UGA), fromLast = TRUE)
-UGA <- subset(UGA, select = -DATE)
-UGA.xts <- xts(UGA[,-1], order.by = UGA$Date)
+UGA <- UGA[!(UGA$ROLL == 1),]
+# UGA <- UGA %>% 
+#   mutate(Date = as.Date(DATE)) %>% 
+#   complete(Date = seq.Date(min(DATE), max(DATE), by="day"))
+# UGA <- na.locf(na.locf(UGA), fromLast = TRUE)
+# UGA <- subset(UGA, select = -DATE)
+UGA.xts <- xts(UGA[,-1], order.by = UGA$DATE)
+
+#---- LM OLS Models
+corn.ols <- lm(CORN$per_ETF_return ~ CORN$per_asset_return)
+CORN$etf_asset_error <- corn.ols$residuals  # The residuals of the model are the tracking error
+summary(corn.ols)
+adf.test(corn.ols$residuals)
+
+soyb.ols <- lm(SOYB$per_ETF_return ~ SOYB$per_asset_return)
+SOYB$etf_asset_error <- soyb.ols$residuals 
+summary(soyb.ols)
+adf.test(soyb.ols$residuals)
+
+weat.ols <- lm(WEAT$per_ETF_return ~ WEAT$per_asset_return)
+WEAT$etf_asset_error <- weat.ols$residuals
+summary(weat.ols)
+adf.test(soyb.ols$residuals)
+
+uso.ols <- lm(USO$per_ETF_return ~USO$per_asset_return)
+USO$etf_asset_error <- uso.ols$residuals
+summary(uso.ols)
+adf.test(uso.ols$residuals)
+
+uga.ols <- lm(UGA$per_ETF_return ~ UGA$per_asset_return)
+UGA$etf_asset_error <- uga.ols$residuals
+summary(uga.ols)
+adf.test(uga.ols$residuals)
 
 #--- ETF Returns
+
 par(mfrow = c(3, 2), mai = c(0.25, 0.5, 0.2, 0.05))
-plot(CORN$Date, CORN$per_ETF_return, type = "l", main = "CORN",
+plot(CORN$DATE, CORN$per_ETF_return, type = "l", main = "CORN",
      xlab = "", ylab = "", ylim = c(-10, 10))
-plot(SOYB$Date, SOYB$per_ETF_return, type = "l", main = "SOYB",
+plot(SOYB$DATE, SOYB$per_ETF_return, type = "l", main = "SOYB",
      xlab = "", ylab = "", ylim = c(-10, 10))
-plot(WEAT$Date, WEAT$per_ETF_return, type = "l", main = "WEAT",
+plot(WEAT$DATE, WEAT$per_ETF_return, type = "l", main = "WEAT",
      xlab = "", ylab = "Percent Return", ylim = c(-10, 10))
 plot(USO$DATE, USO$per_ETF_return, type = "l", main = "USO", 
      xlab = "", ylab = "", ylim = c(-10, 10))
-plot(UGA$Date, UGA$per_ETF_return, type = "l", main = "UGA",
+plot(UGA$DATE, UGA$per_ETF_return, type = "l", main = "UGA",
      xlab = "", ylab = "", ylim = c(-10, 10))
 
 #-- Asset Returns
 par(mfrow = c(3, 2), mai = c(0.25, 0.5, 0.2, 0.05))
-plot(CORN$Date, CORN$per_asset_return, type = "l", main = "CORN",
+plot(CORN$DATE, CORN$per_asset_return, type = "l", main = "CORN",
      xlab = "", ylab = "", ylim = c(-10, 10))
-plot(SOYB$Date, SOYB$per_asset_return, type = "l", main = "SOYB",
+plot(SOYB$DATE, SOYB$per_asset_return, type = "l", main = "SOYB",
      xlab = "", ylab = "", ylim = c(-10, 10))
-plot(WEAT$Date, WEAT$per_asset_return, type = "l", main = "WEAT",
+plot(WEAT$DATE, WEAT$per_asset_return, type = "l", main = "WEAT",
      xlab = "", ylab = "Percent Return", ylim = c(-10, 10))
 plot(USO$DATE, USO$per_asset_return, type = "l", main = "USO", 
      xlab = "", ylab = "", ylim = c(-10, 10))
-plot(UGA$Date, UGA$per_asset_return, type = "l", main = "UGA",
+plot(UGA$DATE, UGA$per_asset_return, type = "l", main = "UGA",
      xlab = "", ylab = "", ylim = c(-10, 10))
 
 #--Tracking Error
 par(mfrow = c(3, 2), mai = c(0.25, 0.5, 0.2, 0.05))
-plot(CORN$Date, CORN$etf_asset_error, type = "l", main = "CORN",
+plot(CORN$DATE, CORN$etf_asset_error, type = "l", main = "CORN",
      xlab = "", ylab = "", ylim = c(-5, 5))
-plot(SOYB$Date, SOYB$etf_asset_error, type = "l", main = "SOYB",
+plot(SOYB$DATE, SOYB$etf_asset_error, type = "l", main = "SOYB",
      xlab = "", ylab = "", ylim = c(-5, 5))
-plot(WEAT$Date, WEAT$etf_asset_error, type = "l", main = "WEAT",
+plot(WEAT$DATE, WEAT$etf_asset_error, type = "l", main = "WEAT",
      xlab = "", ylab = "Percent Error", ylim = c(-5, 5))
 plot(USO$DATE, USO$etf_asset_error, type = "l", main = "USO", 
      xlab = "", ylab = "", ylim = c(-5, 5))
-plot(UGA$Date, UGA$etf_asset_error, type = "l", main = "UGA",
+plot(UGA$DATE, UGA$etf_asset_error, type = "l", main = "UGA",
      xlab = "", ylab = "", ylim = c(-5, 5))
 
 
@@ -265,12 +298,22 @@ plot(USO$DATE, USO$etf_asset_error^2, type = "l", main = "USO",
 plot(UGA$Date, UGA$etf_asset_error^2, type = "l", main = "UGA",
      xlab = "", ylab = "")
 
+#-------- FIT ARIMA Model to Tracking Error
+arima100 <- arima(UGA$etf_asset_error, order = c(2,0,2))
+summary(arima100)
+checkresiduals(arima100)
+Box.test(arima100$residuals^2, type = 'Ljung-Box')
+#auto.arima(WEAT$etf_asset_error)
+
 
 # Base Garch Models
 corn.base_model_spec <- ugarchspec(variance.model = list(garchOrder = c(1,1)),
-                              mean.model = list(armaOrder = c(3,0)))
+                              mean.model = list(armaOrder = c(1,2)))
 corn.base_fit <- ugarchfit(data = CORN.xts$etf_asset_error, spec = corn.base_model_spec)
 corn.base_fit
+
+robust_coef <- as.data.frame(corn.base_fit@fit[['robust.matcoef']])
+write_xlsx(robust_coef, "data.xlsx")
 
 soyb.base_model_spec <- ugarchspec(variance.model = list(garchOrder = c(1,1)),
                               mean.model = list(armaOrder = c(4,3)))
@@ -283,12 +326,12 @@ weat.base_fit <- ugarchfit(data = WEAT.xts$etf_asset_error, spec = weat.base_mod
 weat.base_fit
 
 uso.base_model_spec <- ugarchspec(variance.model = list(garchOrder = c(1,1)),
-                              mean.model = list(armaOrder = c(2,1)))
+                              mean.model = list(armaOrder = c(2,2)))
 uso.base_fit <- ugarchfit(data = USO.xts$etf_asset_error, spec = uso.base_model_spec)
 uso.base_fit
 
 uga.base_model_spec <- ugarchspec(variance.model = list(garchOrder = c(1,1)),
-                              mean.model = list(armaOrder = c(3,1)))
+                              mean.model = list(armaOrder = c(2,2)))
 uga.base_fit <- ugarchfit(data = UGA.xts$etf_asset_error, spec = uga.base_model_spec)
 uga.base_fit
 
@@ -385,6 +428,13 @@ ggAcf(uga.s)
 ## For each commodity, I have two external variable xts objects, one for the 
 ## mean, and one for the variance equation
 
+ols <- lm( CORN$per_ETF_return ~ CORN$per_asset_return)
+summary(ols)
+plot(CORN$Date, ols$residuals, type = 'l')
+
+tsdisplay(ols$residuals)
+ggAcf(ols$residuals)
+
 CORN.mean.ext_reg <- CORN.xts[, c('per_asset_return', 'volume_return')]
 
 
@@ -455,26 +505,26 @@ robust_coef <- as.data.frame(corn.full_fit@fit[['robust.matcoef']])
 write_xlsx(robust_coef, "data.xlsx")
 
 
-# SOYB
-#SOYB.mean.ext_reg <- SOYB.xts[, c('per_asset_return')]
-#SOYB.mean.ext_reg <- as.matrix(SOYB.mean.ext_reg)
-# 
-# SOYB.var.ext_reg <- SOYB.xts[, c('S WASDE', 'S WASDE + CP', 'S Grain Stocks', 'S Prospective Plantings', 'S Acreage Report', 
-#                                  'S Cattle on Feed', 'S Hogs & Pigs', 'S Day Before Roll', 'S Day After Roll', 'per_asset_return',
-#                                  'volume_return')]
-# 
-# soyb.full_model_spec <- ugarchspec(mean.model = list(armaOrder = c(4,3),include.mean = TRUE), 
-#                                    variance.model = list(garchOrder = c(1,1),
-#                                                          external.regressors = SOYB.var.ext_reg))
-# 
-# 
-# setbounds(soyb.full_model_spec) <- list(vxreg1 = c(-100,100), vxreg2 = c(-100,100), vxreg3 = c(-100,100), vxreg4 = c(-100,100),
-#                                         vxreg5 = c(-100,100), vxreg6 = c(-100,100), vxreg7 = c(-100,100), vxreg8 = c(-100,100),
-#                                         vxreg9 = c(-100,100), vxreg10 = c(-100,100), mxreg1 = c(-100,100), mxreg2 = c(-100,100), mxreg3 = c(-100,100),
-#                                         mxreg4 = c(-100,100))
-# 
-# soyb.full_fit <- ugarchfit(data = SOYB.xts$etf_asset_error, spec = soyb.full_model_spec)
-# soyb.full_fit
+SOYB
+SOYB.mean.ext_reg <- SOYB.xts[, c('per_asset_return')]
+SOYB.mean.ext_reg <- as.matrix(SOYB.mean.ext_reg)
+
+SOYB.var.ext_reg <- SOYB.xts[, c('S WASDE', 'S WASDE + CP', 'S Grain Stocks', 'S Prospective Plantings', 'S Acreage Report',
+                                 'S Cattle on Feed', 'S Hogs & Pigs', 'S Day Before Roll', 'S Day After Roll', 'per_asset_return',
+                                 'volume_return')]
+
+soyb.full_model_spec <- ugarchspec(mean.model = list(armaOrder = c(4,3),include.mean = TRUE),
+                                   variance.model = list(garchOrder = c(1,1),
+                                                         external.regressors = SOYB.mean.ext_reg))
+
+
+setbounds(soyb.full_model_spec) <- list(vxreg1 = c(-100,100), vxreg2 = c(-100,100), vxreg3 = c(-100,100), vxreg4 = c(-100,100),
+                                        vxreg5 = c(-100,100), vxreg6 = c(-100,100), vxreg7 = c(-100,100), vxreg8 = c(-100,100),
+                                        vxreg9 = c(-100,100), vxreg10 = c(-100,100), mxreg1 = c(-100,100), mxreg2 = c(-100,100), mxreg3 = c(-100,100),
+                                        mxreg4 = c(-100,100))
+
+soyb.full_fit <- ugarchfit(data = SOYB.xts$etf_asset_error, spec = soyb.full_model_spec)
+soyb.full_fit
 
 
 # WEAT
